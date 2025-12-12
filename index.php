7722bb59fc3ad51470cfb296f9eb444f812df50f
lifpro <ysalissou@gmail.com>
2025-12-12 18:42:08 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+  <title>Technolab QUIZZ</title>
+</head>
+
+<body>
+  <h1><?php echo "Bienvenue sur le QUIZZ de Technolab"; ?></h1>
+  <p><?php echo "Testez vos connaissances avec nos quiz passionnants !"; ?></p>
+  <p> <?php echo "La date actuelle est " . date(" d/m/Y H:i:s"); ?></p>
+</body>
+
+</html>
